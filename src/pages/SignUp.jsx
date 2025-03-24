c5876c35a62835f62094c55b1e939c29272e54fd
Hujaifa81 <abuhojaifa123@gmail.com>
2025-03-25 00:03:28 +0600

    sign in  complete

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../providers/AuthProvider';
 import { updateProfile } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const {emailRegister}=useContext(AuthContext)
@@ -37,7 +37,7 @@ const SignUp = () => {
     };
     return (
         <div className="max-w-xl mx-auto p-5">
-            <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+            <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                 <div>
                     <label className="block font-medium">Name</label>
@@ -66,7 +66,7 @@ const SignUp = () => {
                     {errors.password && <p className="text-red-500">{errors.password.message}</p>}
                 </div>
                 <div>
-                    <label className="block font-medium">Photo</label>
+                    <label className="block font-medium">Photo URL</label>
                     <input type="url" className="w-full border p-2 rounded"
                         {...register("photo", {
                             required: "Photo URL is required",
@@ -75,7 +75,8 @@ const SignUp = () => {
                     {errors.photo && <p className="text-red-500">{errors.photo.message}</p>}
                 </div>
 
-                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Sign Up</button>
+                <button type="submit" className="bg-red-600 text-white px-4 py-2 rounded w-full">Sign Up</button>
+                <p>Already have an account?<span className='text-red-600'><Link to='/sign-in'>Sign in</Link></span></p>
             </form>
         </div>
     );
